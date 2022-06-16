e989fb1975785345c3f3af10ce79af6cb2aaae4f
zsj <zhangsjup@163.com>
2022-06-16 11:32:45 +0800

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,7 @@
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation 2</a
+        >vue-cli documentation</a
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
